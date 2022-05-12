3a4fa0292a180ebdfb0cbddab532b0070b93e079
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-05-12 21:04:45 +0530

    feat:created the basic pages

---
+++ TelePlay/src/Pages/index.js
@@ -0,0 +1,7 @@
+export {PageNotFound} from './Error';
+export {Explore} from './Explore';
+export {History} from './History';
+export {Home} from './Home';
+export {Liked} from './Liked';
+export {Playlist} from './Playlist';
+export {WatchLater} from './WatchLater';
